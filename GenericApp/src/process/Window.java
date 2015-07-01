3c02f6460efe852623dcf45f120f5472d38d9d8a
Lucas J. Couchot <lucas.couchot@gmail.com>
2015-07-01 03:10:17 -0300

    Agregué la lógica para poder levantar properties de un archivo. Por ahora sólo tiene el nombre del archivo de configuración (Esto sirve para no tener que recompilar la aplicación para poder usar otra configuración)

--- GenericApp/src/process/Window.java
+++ GenericApp/src/process/Window.java
@@ -1,6 +1,10 @@
 package process;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
@@ -9,19 +13,58 @@ public class Window
 {
 	private static final Logger logger = Logger.getLogger(Window.class.getName());
 	
+	private static final String PROPERTIES_DEFAULT_NAME = "main.application.properties";
+	private static final String DEFAULT_CONFIG_FILE_NAME = "config.xml";
+	
 	public static void main(String[] args) 
     {
 		//Configuro el logger global (afecta a los loggers de todas las clases) - También podría setearle un handler para que loguee en un archivo
 		LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME).setLevel(Level.FINE);
 		
-		String FileName = "config.xml";
+		//levanto properties si existen
+		Properties properties = loadProperties();
+		
+		String fileName = DEFAULT_CONFIG_FILE_NAME;
+		
+		//en caso de existir la property, cambio el valor por defecto del nombre del archivo de config
+		if(properties.containsKey("config.file")) {
+			fileName = (String) properties.get("config.file");
+		}
+		
         //Log.setLogInfo(Log.INFO);
         //Log.writeLogMessage(Log.INFO, "Archivo = " + FileName);
-		logger.info("Archivo = " + FileName);
+		logger.info("Archivo = " + fileName);
 		
-		XMLProcess xmlProcess = new XMLProcess(FileName);
+		XMLProcess xmlProcess = new XMLProcess(fileName);
+		
         ArrayList<Aplicacion> applications = xmlProcess.parseDocument();
+        
         new MainWindow(applications);
     }
-
+	
+	private static Properties loadProperties() {
+		
+		Properties properties = new Properties();
+		
+		try {
+			//abro archivo de properties
+			FileInputStream in = new FileInputStream(PROPERTIES_DEFAULT_NAME);
+			//cargo properties desde el archivo
+			properties.load(in);
+			//cierro archivo
+			in.close();
+		} catch (FileNotFoundException e) {
+			
+			logger.warning("Archivo de properties: " + PROPERTIES_DEFAULT_NAME + " no encontrado. Se seguirá con configuración por defecto.");
+			return properties;
+		} catch (IOException e) {
+			
+			logger.warning("Error al cargar archivo de properties: " + PROPERTIES_DEFAULT_NAME + ". Se seguirá con configuración por defecto.");
+			return properties;
+		}
+		
+		logger.info("Properties cargadas correctamente!");
+			
+		return properties;
+	}
 }
